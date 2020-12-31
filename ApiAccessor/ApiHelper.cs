09d0a47171116f53a1f3eabf8f3df623ae3ae289
RuslanPr0g <theruslan.prog@gmail.com>
2020-12-31 14:31:31 +0300

    install webapi client

---
+++ ApiAccessor/ApiHelper.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApiAccessor
+{
+    public class ApiHelper
+    { // https://api.agify.io/?name=michael
+
+    }
+}
