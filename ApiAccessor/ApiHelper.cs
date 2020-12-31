4d22a6241467b3a64ceda02cf84085d450cd3a79
RuslanPr0g <theruslan.prog@gmail.com>
2020-12-31 14:46:42 +0300

    setup api helper class

--- ApiAccessor/ApiHelper.cs
+++ ApiAccessor/ApiHelper.cs
@@ -1,11 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 
 namespace ApiAccessor
 {
     public class ApiHelper
-    { // https://api.agify.io/?name=michael
+    {
+        public static HttpClient ApiClient { get; set; } // statis because in this case it will be one per app
 
+        public static void InitializeClient()
+        {
+            ApiClient = new HttpClient();
+            ApiClient.BaseAddress = new Uri("https://api.agify.io/");
+            ApiClient.DefaultRequestHeaders.Accept.Clear();
+            ApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
     }
 }
